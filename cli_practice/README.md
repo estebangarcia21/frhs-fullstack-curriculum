712d919835afb996cfbb9bded735cb175eb7db56
Esteban Garcia <estebangarcia2121@gmail.com>
2021-09-19 18:21:26 -0700

    Added CLI practice and verify script
    Verify script checks that the practices were done correctly.

---
+++ cli_practice/README.md
@@ -0,0 +1,54 @@
+# Practice the common CLI commands!
+
+Create a directory that in the following format. Then create the file structure.
+
+```
+$ mkdir <title>
+```
+
+Verify you work using the `verify.js` script. For example, I want to verify the `bundle` file structure is correct.
+
+Open a terminal window. `View > Terminal`
+
+```
+$ cd cli_practice
+$ node check.js bundle
+```
+
+Make sure you are in the `cli_practice` directory! Confirm using `pwd`.
+
+## 0
+
+Create a file structure that looks like this...
+
+```
+0/
+├─ secret/
+│  ├─ somethingSecret.txt
+├─ a.txt
+```
+
+## 1
+
+```
+1/
+├─ a
+├─ b.js
+├─ c.sh
+├─ x/
+│  ├─ y/
+│  │  ├─ z.o
+```
+
+## bundle
+
+```
+bundle/
+├─ webpack.config.js
+├─ yarn.lock
+├─ package.json
+├─ src/
+│  ├─ index.html
+│  ├─ main.js
+│  ├─ styles.css
+```
---
+++ cli_practice/check.js
@@ -0,0 +1,145 @@
+// @ts-check
+
+/**
+ * Verify if you did one of the try-its correctly.
+ */
+
+const { existsSync } = require('fs');
+const { argv } = require('process');
+const path = require('path');
+
+/**
+ * Exit the program with a message.
+ * @param {*} msg The message to display.
+ */
+function exitWithMessage(msg) {
+  console.log(msg);
+  process.exit(1);
+}
+
+/**
+ * Format an array as a list.
+ * @param {Array<string>} arr The array to format.
+ * @returns {string} The formatted list.
+ */
+function fmtList(arr) {
+  return '\n' + arr.map((i) => `- ${i}`).join('\n');
+}
+
+/**
+ *
+ * @param {string} name
+ * @param {Array} items
+ */
+function directory(name, items) {
+  return {
+    directory: name,
+    items
+  };
+}
+
+/**
+ *
+ * @param {string} name
+ */
+function file(name) {
+  return {
+    file: name
+  };
+}
+
+/**
+ * Append exampleId to the begging of a path
+ * @param {string} path The path to prepend the exampleId to.
+ * @returns {string} The path with the exampleId prepended.
+ */
+function prependTitle(path) {
+  return `${title}/${path}`;
+}
+
+/**
+ * Check if all files in a directory exist.
+ * @param {string} cwd The current working directory.
+ * @param {array} tree The tree to traverse.
+ * @returns {Array} An array of missing files.
+ */
+function traverseTree(cwd, tree) {
+  const missingFiles = [];
+
+  for (const item of tree) {
+    const dir = item.directory;
+
+    if (dir) {
+      const files = traverseTree(path.join(cwd, dir), item.items);
+      missingFiles.push(...files);
+      continue;
+    }
+
+    if (item.file) {
+      if (!existsSync(path.join(cwd, item.file))) {
+        missingFiles.push(
+          path.join(cwd.split('/').slice(1).join('/'), item.file)
+        );
+      }
+    }
+  }
+
+  return missingFiles;
+}
+
+// prettier-ignore
+// The file trees to check against.
+const trees = {
+  0: [
+    directory('secret', [
+      file('somethingSecret.txt')
+    ]),
+    file('a.txt')
+  ],
+  1: [
+    file('a'),
+    file('b.js'),
+    file('c.sh'),
+    directory('x', [
+      directory('y', [
+        file('z.o')
+      ])
+    ])
+  ],
+  bundle: [
+    file('webpack.config.js'),
+    file('yarn.lock'),
+    file('package.json'),
+    directory('src', [
+      file('index.html'),
+      file('main.js'),
+      file('styles.css')
+    ])
+  ]
+};
+
+const title = argv[2] || null;
+
+if (!title) {
+  exitWithMessage('Usage: check.js <title>');
+}
+
+const currentTree = trees[title];
+
+if (!currentTree) {
+  exitWithMessage(
+    `No file tree found for ${title}. These are the possible file trees you can check: ${fmtList(
+      Object.keys(trees)
+    )}`
+  );
+}
+
+const files = traverseTree(title, currentTree);
+
+if (files.length) {
+  exitWithMessage(
+    `Missing files in ${title}: ${fmtList(files)}\n\nTry adding these files!`
+  );
+}
+
+console.log('You did it perfectly! Great job!');
