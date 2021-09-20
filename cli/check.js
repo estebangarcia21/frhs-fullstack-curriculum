694e38f9da5a1838e33c7fe285aaa45535270521
Esteban Garcia <estebangarcia2121@gmail.com>
2021-09-19 19:28:11 -0700

    Cleaned up script

--- cli/check.js
+++ cli/check.js
@@ -12,7 +12,7 @@ const path = require('path');
  * Exit the program with a message.
  * @param {*} msg The message to display.
  */
-function exitWithMessage(msg) {
+function exit(msg) {
   console.log(msg);
   process.exit(1);
 }
@@ -63,15 +63,14 @@ function traverseTree(cwd, tree) {
     if (dir) {
       const files = traverseTree(path.join(cwd, dir), item.items);
       missingFiles.push(...files);
+
       continue;
     }
 
-    if (item.file) {
-      if (!existsSync(path.join(cwd, item.file))) {
-        missingFiles.push(
-          path.join(cwd.split('/').slice(1).join('/'), item.file)
-        );
-      }
+    if (item.file && !existsSync(path.join(cwd, item.file))) {
+      missingFiles.push(
+        path.join(cwd.substring(cwd.indexOf('/') + 1), item.file)
+      );
     }
   }
 
@@ -109,28 +108,32 @@ const trees = {
   ]
 };
 
-const title = argv[2] || null;
+function main() {
+  const title = argv[2] || null;
 
-if (!title) {
-  exitWithMessage('Usage: check.js <title>');
+  if (!title) {
+    exit('Usage: check.js <title>');
+  }
+
+  const currentTree = trees[title];
+
+  if (!currentTree) {
+    exit(
+      `No file tree found for ${title}. These are the possible file trees you can check: ${fmtList(
+        Object.keys(trees)
+      )}`
+    );
+  }
+
+  const files = traverseTree(title, currentTree);
+
+  if (files.length) {
+    exit(
+      `Missing files in ${title}: ${fmtList(files)}\n\nTry adding these files!`
+    );
+  }
+
+  console.log('You did it perfectly! Great job!');
 }
 
-const currentTree = trees[title];
-
-if (!currentTree) {
-  exitWithMessage(
-    `No file tree found for ${title}. These are the possible file trees you can check: ${fmtList(
-      Object.keys(trees)
-    )}`
-  );
-}
-
-const files = traverseTree(title, currentTree);
-
-if (files.length) {
-  exitWithMessage(
-    `Missing files in ${title}: ${fmtList(files)}\n\nTry adding these files!`
-  );
-}
-
-console.log('You did it perfectly! Great job!');
+main();
