87c150667ce17d6626a68dea56cceb3d773ef8b2
Esteban Garcia <estebangarcia2121@gmail.com>
2021-10-10 21:31:34 -0700

    Added basic js notes

---
+++ javascript/basics_01.js
@@ -0,0 +1,179 @@
+/*
+ * =============================
+ * Comments
+ * =============================
+ */
+{
+  // Single line comment. Start the line with double forward slash. You can write anything and it will be completley ignored by the compiler!
+  /*
+   * Multi line comment.
+   * You can make this as many lines as you want.
+   *
+   * Start the line with a forward slash and a star.
+   * You can write anything and it will be completley ignored by the compiler!
+   *
+   * You can also use the backslash to escape the star.
+   * End the multiline comment with a star and a forward slash.
+   */
+}
+
+/*
+ * =============================
+ * Variables
+ * =============================
+ */
+{
+  // Declare a variable using the var keyword and the variable name.
+  // Variable names should always be camelCase. Start the variable name with a lower case letter
+  // and each new word with a capital letter.
+  var myVariable;
+
+  // Set the value of a variable with the assignment operator (=).
+  myVariable = 64;
+
+  // You can declare and initialize a variable at the same time.
+  var myOtherVariable = 'Initialized!';
+
+  // Different types of values in JavaScript. Numbers, strings, booleans, objects, and arrays are the most common.
+
+  // A number value. Can be an integer or decimal.
+  var numberValueInteger = 0;
+  var numberValueDecimal = -3.5;
+
+  // A string is a sequence of characters. You can use single or double quotes.
+  var stringValue = 'Hello world!';
+
+  // Boolean values are either true or false. Can also be represented with a 0 for false and 1 for true.
+  var booleanValueFalse = false;
+  var booleanValueTrue = true;
+
+  // A map of values indexed by a string key or position in an array.
+  var object = {};
+
+  // A set of values.
+  var array = [];
+  var exampleArray = [0, 1, 2];
+  var exampleArray2 = ['Hello', 'world', 0, {}, []];
+}
+
+/*
+ * =============================
+ * Logging
+ * =============================
+ */
+{
+  // Use console.log to log a value to the console.
+  console.log('Hello world!');
+}
+
+/*
+ * =============================
+ * Loops
+ * =============================
+ */
+{
+  // You can repeatedly run a block of code a number of times using a loop!
+  // Specifiy the amonut of times you want to loop. This loop loops five times.
+  for (var i = 0; i < 5; i++) {
+    console.log('Looping! ' + i);
+  }
+}
+
+/*
+ * =============================
+ * Conditionals
+ * =============================
+ */
+{
+  // You can control the flow of your code using conditional statements. If something is true, then run a block of code.
+  if (true) {
+    console.log('This is true!');
+  }
+
+  // Lets check some stuff out.
+  // Use the === operator to check if two values are equal.
+  if (2 === 2) {
+    console.log('Two is equal to two!');
+  }
+
+  // You can use else to run something if a conditional evaluates to false.
+  if (2 === 3) {
+    console.log('Two is equal to three!');
+  } else {
+    console.log('Two is not equal to three!');
+  }
+
+  // You can also use else if to check for multiple conditions.
+  if (2 === 3) {
+    console.log('Two is equal to three!');
+  } else if (2 === 2) {
+    console.log('Two is equal to two!');
+  }
+
+  // All three...
+  if (2 === 3) {
+    console.log('Two is equal to three!');
+  } else if (2 === 2) {
+    console.log('Two is equal to two!');
+  } else {
+    console.log('Bruh...');
+  }
+
+  // You can also use the ternary operator to check for a condition and run a block of code if it is true.
+  const isTwoEqualToTwo =
+    2 === 2 ? 'This is the value if its true!' : 'Value if its not true!';
+
+  console.log('(Ternary) isTwoEqualToTwo: ' + isTwoEqualToTwo);
+
+  // All conditional operators.
+  // less than / greater than
+  if (2 < 3 || 2 > 3) {
+    console.log('Two is less than three!');
+  }
+
+  // Less than or equal to / greater than or equal to.
+  if (2 <= 3 || 2 >= 3) {
+    console.log('Two is less than or equal to three!');
+  }
+
+  // Not equal to
+  if (2 !== 3) {
+    console.log('Two is not equal to three!');
+  }
+
+  // You can use the AND (&&) operator to check that one condition AND another condition is true.
+  if (2 === 2 && 2 !== 3) {
+    console.log('Two is equal to two and two is equal to three!');
+  }
+
+  // You can use the OR (||) operator to check that one condition OR another condition is true.
+  if (2 === 2 || 2 !== 3) {
+    console.log('Two is equal to two or two is not equal to three!');
+  }
+}
+
+/*
+ * =============================
+ * Objects
+ * =============================
+ */
+{
+  // JSON objects map a stringifiable key to a value. JSON stands for Javascript Object Notation.
+  var user = {
+    firstName: 'Bob',
+    lastName: 'Brown'
+  };
+
+  // Output: {firstName:'Bob',lastName:'Brown'}
+  console.log(user);
+
+  // Access values using dot notation.
+  console.log(user.firstName);
+
+  // Access values using bracket notation.
+  console.log(user['lastName']);
+
+  // Acess the nth key of the object using an integer.
+  console.log(user[0]); // firstName is the 0th key in object.
+  console.log(user[1]); // lastName is the 1st key in the object.
+}
